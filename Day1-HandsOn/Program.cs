ac8e464cdbb5f3693796c1dbf85947300fa7f119
santuparsi <santuparsi@gmail.com>
2022-10-21 14:48:21 +0530

    new_commit

---
+++ Day1-HandsOn/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day1_HandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 12, 23, 34, 45, 56, 67, 78 };
+            //sum or array nos;
+            int sum = n.Sum();
+            Console.WriteLine(sum);
+            //sum of even values
+            sum = n.Where(x => x % 2 == 0).Sum();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
