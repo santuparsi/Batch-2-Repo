9e2d1c01b930481e34ef1b54e26b127cee8d1385
santuparsi <santuparsi@gmail.com>
2022-10-26 15:30:20 +0530

    new_commit

---
+++ API/MyFirstRestAPI/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        //controller actions
+        [HttpGet,Route("GetStudents")]
+        public IActionResult GetStudents()
+        {
+            try
+            {
+                string[] s = { "Rohan", "Nihal", "Manoj" };
+                return StatusCode(200, s);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+                
+            }
+        }
+        [HttpGet,Route("GetStudent/{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            try
+            {
+               
+                return StatusCode(200, "Monish");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+        }
+        [HttpPost,Route("AddStudent")]
+        public IActionResult Add(object student)
+        {
+            try
+            {
+
+                return StatusCode(200, "Added Monsih");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+        }
+    }
+}
